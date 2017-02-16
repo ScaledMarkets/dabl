b3c7bd923bede1a3bcd0c9c9fc808c5ece4b157f
Cliff Berg <cliff@cliffberg.com>
2017-02-16 06:37:50 -0500

    misc

---
+++ test/features/scaledmarkets/dabl/test/TestNumberFormats.feature
@@ -0,0 +1,43 @@
+# language: en
+
+Feature: TestNumberFormats
+	
+	@done
+	Scenario: Test whole number
+		When 
+		Then 
+	
+	@done
+	Scenario: Test negative whole number
+		When 
+		Then 
+	
+	@done
+	Scenario: Test decimal number
+		When 
+		Then 
+	
+	@done
+	Scenario: Test negative decimal number
+		When 
+		Then 
+	
+	@done
+	Scenario: Test simple numeric expression
+		When 
+		Then 
+	
+	@done
+	Scenario: Test simple numeric expression with a negative number
+		When 
+		Then 
+	
+	@done
+	Scenario: Test numeric pattern with two decmial points
+		When 
+		Then 
+	
+	@done
+	Scenario: Test numeric pattern with a wildcard
+		When 
+		Then 
---
+++ test/steps/scaledmarkets/dabl/test/TestNumberFormats.java
@@ -0,0 +1,91 @@
+package scaledmarkets.dabl.test;
+
+import cucumber.api.Format;
+import cucumber.api.java.Before;
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import scaledmarkets.dabl.main.*;
+import scaledmarkets.dabl.node.*;
+
+import java.io.Reader;
+import java.io.StringReader;
+import scaledmarkets.dabl.node.*;
+import java.util.List;
+import java.util.LinkedList;
+
+public class TestNumberFormats extends TestBase {
+	
+	Reader reader;
+
+	@Before
+	public void beforeEachScenario() throws Exception {
+		reader = null;
+	}
+	
+
+	// Test whole number
+	
+	@When("^$")
+	public void () throws Throwable {
+		throw new Exception();
+	}
+	
+	
+	// Test negative whole number
+	
+	@When("^$")
+	public void () throws Throwable {
+		throw new Exception();
+	}
+	
+	
+	// Test decimal number
+	
+	@When("^$")
+	public void () throws Throwable {
+		throw new Exception();
+	}
+	
+	
+	// Test negative decimal number
+	
+	@When("^$")
+	public void () throws Throwable {
+		throw new Exception();
+	}
+	
+	
+	// Test simple numeric expression
+	
+	@When("^$")
+	public void () throws Throwable {
+		throw new Exception();
+	}
+	
+	
+	// Test simple numeric expression with a negative number
+	
+	@When("^$")
+	public void () throws Throwable {
+		throw new Exception();
+	}
+	
+	
+	// Test numeric pattern with two decmial points
+	
+	@When("^$")
+	public void () throws Throwable {
+		throw new Exception();
+	}
+	
+	
+	// Test numeric pattern with a wildcard
+
+	@When("^$")
+	public void () throws Throwable {
+		throw new Exception();
+	}
+}
