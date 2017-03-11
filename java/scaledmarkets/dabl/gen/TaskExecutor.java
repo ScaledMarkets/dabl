8bda914e8f5596db467c5312dd579ca38d0ea161
Cliff Berg <cliff@cliffberg.com>
2017-03-11 13:40:01 -0500

    misc

--- java/scaledmarkets/dabl/gen/TaskExecutor.java
+++ java/scaledmarkets/dabl/gen/TaskExecutor.java
@@ -2,6 +2,10 @@ package scaledmarkets.dabl.gen;
 
 /**
  * For executing a DABL task inside of a container.
+ * When a container is started by a TaskContainerFactory, the container is
+ * given a TaskExecutor to run. The arguments to the TaskExecutor tell the
+ * TaskExecutor where to find the definition of the Task that the TaskExecutor
+ * is to run.
  */
 public class TaskExecutor {
 	
