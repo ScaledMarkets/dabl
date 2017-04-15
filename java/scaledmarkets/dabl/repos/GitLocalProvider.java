f2a18450878f6329ec180d7628bc35e58134b0aa
Cliff Berg <cliff@cliffberg.com>
2017-04-15 12:00:09 -0400

    misc

--- java/scaledmarkets/dabl/repos/GitLocalProvider.java
+++ java/scaledmarkets/dabl/repos/GitLocalProvider.java
@@ -4,18 +4,16 @@ import javax.ws.rs.core.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.client.*;
 
-public class GitRemoteProvider implements RepoProvider {
+public class GitLocalProvider implements RepoProvider {
 	
 	public Repo getRepo(String scheme, path, userid, password) throws Exception {
 		
-		return new GitRemoteRepo(....);
+		return new GitLocalRepo(....);
 	}
 	
-	class GitRemoteRepo extends Repo {
+	class GitLocalRepo extends Repo {
 		
 		// References:
-		// https://developer.github.com/v3/repos/
-		// https://developer.github.com/v3/repos/contents/
 		
 		public void getFiles(String project, List<String> includePatterns,
 			List<String> excludePatterns) throws Exception {
@@ -26,12 +24,6 @@ public class GitRemoteProvider implements RepoProvider {
 				
 			}
 			
-			....
-			ClientBuilder clientBuilder = ClientBuilder.newBuilder().withConfig(clientConfig);
-			
-			Client client = clientBuilder.build();
-			
-			....client.target(uri)
 		}
 		
 		public void putFiles(String project, List<String> includePatterns,
@@ -40,6 +32,6 @@ public class GitRemoteProvider implements RepoProvider {
 			....
 		}
 		
-		protected RepoProvider getRepoProvider() { return GitRemoteProvider.this; }
+		protected RepoProvider getRepoProvider() { return GitLocalProvider.this; }
 	}
 }
--- java/scaledmarkets/dabl/repos/GitRemoteProvider.java
+++ java/scaledmarkets/dabl/repos/GitRemoteProvider.java
@@ -1,8 +1,39 @@
 package scaledmarkets.dabl.repos;
 
+import javax.ws.rs.core.*;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.client.*;
+
 public class GitRemoteProvider implements RepoProvider {
 	
 	public Repo getRepo(String scheme, path, userid, password) throws Exception {
 		
+		return new GitRemoteRepo(....);
+	}
+	
+	class GitRemoteRepo extends Repo {
+		
+		// References:
+		// https://developer.github.com/v3/repos/
+		// https://developer.github.com/v3/repos/contents/
+		
+		public void getFiles(String project, List<String> includePatterns,
+			List<String> excludePatterns) throws Exception {
+			
+			....
+			ClientBuilder clientBuilder = ClientBuilder.newBuilder().withConfig(clientConfig);
+			
+			Client client = clientBuilder.build();
+			
+			....client.target(uri)
+		}
+		
+		public void putFiles(String project, List<String> includePatterns,
+			List<String> excludePatterns) throws Exception {
+			
+			....
+		}
+		
+		protected RepoProvider getRepoProvider() { return GitRemoteProvider.this; }
 	}
 }
