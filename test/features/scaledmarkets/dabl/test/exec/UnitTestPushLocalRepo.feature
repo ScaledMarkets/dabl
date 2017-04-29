0bdd799fa95e6c64d22de74f4b44b81b3e949cdc
Cliff Berg <cliff@cliffberg.com>
2017-04-29 10:06:46 -0400

    misc

--- test/features/scaledmarkets/dabl/test/exec/UnitTestPushLocalRepo.feature
+++ test/features/scaledmarkets/dabl/test/exec/UnitTestPushLocalRepo.feature
@@ -4,43 +4,43 @@
 Feature: UnitTestPushLocalRepo
 	
 	@done
-	Scenario: A single include pattern to push
-		Given there are two files in a directory, a.txt and b.txt
+	Scenario: 1: A single include pattern to push
+		Given there are two files in directory 1, a.txt and b.txt
 		When the include pattern specifies b.txt
 		Then only file b.txt is pushed
 	
 	@done
-	Scenario: A single exclude pattern to push
-		Given there are two files in a directory, a.txt and b.txt
+	Scenario: 2; A single exclude pattern to push
+		Given there are two files in directory 2, a.txt and b.txt
 		When the exclude pattern specifies b.txt
 		Then no files are pushed
 	
 	@done
-	Scenario: One include pattern, and exclude one file, to push
-		Given there are two files in a directory, a.txt and b.txt
+	Scenario: 3: One include pattern, and exclude one file, to push
+		Given there are two files in directory 3, a.txt and b.txt
 		When the include pattern specifies a.txt and b.txt, and the exclude pattern specifies b.txt
 		Then only file a.txt is pushed
 	
 	@done
-	Scenario: One include pattern, and exclude files that match a specified extension to push
-		Given a directory contains files a.txt, b.txt, a.html, b.html, a.rtf, b.rtf
+	Scenario: 4: One include pattern, and exclude files that match a specified extension to push
+		Given directory 4 contains files a.txt, b.txt, a.html, b.html, a.rtf, b.rtf
 		When the include pattern specifies a.* and the exclude pattern specifies *.txt
 		Then only the files a.html and a.rtf are pushed
 	
 	@done
-	Scenario: Include a directory hierarchy
-		Given a directory with files a.txt, b.txt, d/a.txt, d/b.txt, d/dd/a.txt, d/dd/b.txt
+	Scenario: 5: Include a directory hierarchy
+		Given directory 5 with files a.txt, b.txt, d/a.txt, d/b.txt, d/dd/a.txt, d/dd/b.txt
 		When the include pattern specifies **/a.txt
 		Then the files a.txt, d/a.txt, and d/dd/a.txt are pushed
 	
 	@done
-	Scenario: One include pattern, and exclude a directory to push
-		Given a directory with files a.txt, a.rtf, d/a.txt, d/a.rtf, e/a.txt, e/a.rtf, d/dd/a.txt, d/dd/a.rtf
+	Scenario: 6: One include pattern, and exclude a directory to push
+		Given directory 6 with files a.txt, a.rtf, d/a.txt, d/a.rtf, e/a.txt, e/a.rtf, d/dd/a.txt, d/dd/a.rtf
 		When the include pattern specifies **/*.txt and the exclude pattern specifies e
 		Then the files a.txt, d/a.txt, and d/dd/a.txt are pushed
 	
 	@done
-	Scenario: Two include patterns, and exclude files that match a specified extension to push
-		Given a directory with files a.txt, a.rtf, d/a.txt, d/a.rtf, e/a.txt, e/a.rtf, d/dd/a.txt, d/dd/a.rtf
+	Scenario: 7: Two include patterns, and exclude files that match a specified extension to push
+		Given directory 7 with files a.txt, a.rtf, d/a.txt, d/a.rtf, e/a.txt, e/a.rtf, d/dd/a.txt, d/dd/a.rtf
 		When the include pattern specifies * and ** and the exclude pattern specifies **/*.txt
 		Then the files a.rtf, d/a.rtf, d/dd/a.rtf, e/a.rtf are pushed
--- test/steps/scaledmarkets/dabl/test/exec/UnitTestPushLocalRepo.java
+++ test/steps/scaledmarkets/dabl/test/exec/UnitTestPushLocalRepo.java
@@ -17,8 +17,14 @@ import cucumber.api.java.en.And;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.FileVisitResult
+import java.nio.file.attribute.BasicFileAttributes
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Stream
 
 public class UnitTestPushLocalRepo extends TestBase {
 
@@ -27,10 +33,7 @@ public class UnitTestPushLocalRepo extends TestBase {
 	private static String OutputsName = "my_outputs";
 	private LocalRepo repo;
 	private File basedir = new File("UnitTestPushLocalRepo_scratch");
-	private File given1dir = new File(basedir, "given1");
-	private File given2dir = new File(basedir, "given2");
-	private File given3dir = new File(basedir, "given3");
-	private File given4dir = new File(basedir, "given4");
+	private File givendir;
 	
 	private String base_dabl =
 "namespace " + NamespaceName + " \n" +
@@ -45,18 +48,27 @@ public class UnitTestPushLocalRepo extends TestBase {
 	}
 	
 	
-	// Scenario: A single include pattern to push
+	@Before
+	public void beforeEachScenario() throws Exception {
+	}
+	
+	@After
+	public void afterEachScenario() throws Exception {
+		teardown(this.givendir);
+	}
+	
+
+	// Scenario: 1: A single include pattern to push
 
 	// Given 1:
-	@Given("^there are two files in a directory, a\\.txt and b\\.txt$")
-	public void there_are_two_files_in_a_directory_a_txt_and_b_txt() throws Throwable {
+	@Given("^there are two files in directory (\\d+), a\\.txt and b\\.txt$")
+	public void there_are_two_files_in_a_directory_a_txt_and_b_txt(String dirNum) throws Throwable {
 		
-		this.given1dir.mkdir();
-		this.given1dir.deleteOnExit();
+		setup(dirNum);
 		
 		// Create two files a.txt and b.txt in dir.
-		File filea = new File(given1dir, "a.txt");
-		File fileb = new File(given1dir, "b.txt");
+		File filea = new File(givendir, "a.txt");
+		File fileb = new File(givendir, "b.txt");
 		filea.createNewFile();
 		fileb.createNewFile();
 	}
@@ -81,7 +93,7 @@ public class UnitTestPushLocalRepo extends TestBase {
 	}
 	
 	
-	// Scenario: A single exclude pattern to push
+	// Scenario: 2: A single exclude pattern to push
 
 	@When("^the exclude pattern specifies b\\.txt$")
 	public void the_exclude_pattern_specifies_b_txt() throws Throwable {
@@ -95,10 +107,11 @@ public class UnitTestPushLocalRepo extends TestBase {
 	public void no_files_are_pushed() throws Throwable {
 		long n = this.repo.countAllFiles();
 		assertThat(n == 0, "Found " + n + " files");
+		deleteDirContents(givendir);
 	}
 	
 	
-	// Scenario: One include pattern, and exclude one file, to push
+	// Scenario: 3: One include pattern, and exclude one file, to push
 
 	@When("^the include pattern specifies a\\.txt and b\\.txt, and the exclude pattern specifies b\\.txt$")
 	public void the_include_pattern_specifies_a_txt_and_b_txt_and_the_exclude_pattern_specifies_b_txt() throws Throwable {
@@ -116,21 +129,20 @@ public class UnitTestPushLocalRepo extends TestBase {
 	}
 	
 	
-	// Scenario: One include pattern, and exclude files that match a specified extension to push
+	// Scenario: 4: One include pattern, and exclude files that match a specified extension to push
 
 	// Given 2:
-	@Given("^a directory contains files a\\.txt, b\\.txt, a\\.html, b\\.html, a\\.rtf, b\\.rtf$")
-	public void a_directory_contains_files_a_txt_b_txt_a_html_b_html_a_rtf_b_rtf() throws Throwable {
+	@Given("^directory (\\d+) contains files a\\.txt, b\\.txt, a\\.html, b\\.html, a\\.rtf, b\\.rtf$")
+	public void a_directory_contains_files_a_txt_b_txt_a_html_b_html_a_rtf_b_rtf(int dirNum) throws Throwable {
 		
-		this.given2dir.mkdir();
-		this.given2dir.deleteOnExit();
+		setup(dirNum);
 
-		File filea = new File(given2dir, "a.txt");
-		File fileb = new File(given2dir, "b.txt");
-		File filec = new File(given2dir, "a.html");
-		File filed = new File(given2dir, "b.html");
-		File filee = new File(given2dir, "a.rtf");
-		File filef = new File(given2dir, "b.rtf");
+		File filea = new File(givendir, "a.txt");
+		File fileb = new File(givendir, "b.txt");
+		File filec = new File(givendir, "a.html");
+		File filed = new File(givendir, "b.html");
+		File filee = new File(givendir, "a.rtf");
+		File filef = new File(givendir, "b.rtf");
 		filea.createNewFile();
 		fileb.createNewFile();
 		filec.createNewFile();
@@ -139,8 +151,6 @@ public class UnitTestPushLocalRepo extends TestBase {
 		filef.createNewFile();
 		
 		....continue here: complete the @Given methods.
-		Also, make sure that the directories can be deleted at end of test, given
-		that they are not empty.
 	}
 	
 	@When("^the include pattern specifies a\\.\\* and the exclude pattern specifies \\*\\.txt$")
@@ -160,14 +170,14 @@ public class UnitTestPushLocalRepo extends TestBase {
 	}
 	
 	
-	// Scenario: Include a directory hierarchy
+	// Scenario: 5: Include a directory hierarchy
 
 	// Given 3:
-	@Given("^a directory with files a\\.txt, b\\.txt, d/a\\.txt, d/b\\.txt, d/dd/a\\.txt, d/dd/b\\.txt$")
-	public void a_directory_with_files_a_txt_b_txt_d_a_txt_d_b_txt_d_dd_a_txt_d_dd_b_txt() throws Throwable {
+	@Given("^directory (\\d+) with files a\\.txt, b\\.txt, d/a\\.txt, d/b\\.txt, d/dd/a\\.txt, d/dd/b\\.txt$")
+	public void a_directory_with_files_a_txt_b_txt_d_a_txt_d_b_txt_d_dd_a_txt_d_dd_b_txt(int dirNum) throws Throwable {
 		
-		this.given3dir.mkdir();
-		this.given3dir.deleteOnExit();
+		setup(dirNum);
+		....
 	}
 	
 	@When("^the include pattern specifies \\*\\*/a\\.txt$")
@@ -188,14 +198,14 @@ public class UnitTestPushLocalRepo extends TestBase {
 	}
 	
 	
-	// Scenario: One include pattern, and exclude a directory to push
+	// Scenario: 6: One include pattern, and exclude a directory to push
 
 	// Given 4:
-	@Given("^a directory with files a\\.txt, a\\.rtf, d/a\\.txt, d/a\\.rtf, e/a\\.txt, e/a\\.rtf, d/dd/a\\.txt, d/dd/a\\.rtf$")
-	public void a_directory_with_filex_a_txt_a_rtf_d_a_txt_d_a_rtf_e_a_txt_e_a_rtf_d_dd_a_txt_d_dd_a_rtf() throws Throwable {
+	@Given("^directory (\\d+) with files a\\.txt, a\\.rtf, d/a\\.txt, d/a\\.rtf, e/a\\.txt, e/a\\.rtf, d/dd/a\\.txt, d/dd/a\\.rtf$")
+	public void a_directory_with_filex_a_txt_a_rtf_d_a_txt_d_a_rtf_e_a_txt_e_a_rtf_d_dd_a_txt_d_dd_a_rtf(int dirNum) throws Throwable {
 		
-		this.given4dir.mkdir();
-		this.given4dir.deleteOnExit();
+		setup(dirNum);
+		....
 	}
 	
 	@When("^the include pattern specifies \\*\\*/\\*\\.txt and the exclude pattern specifies e$")
@@ -207,7 +217,7 @@ public class UnitTestPushLocalRepo extends TestBase {
 	}
 	
 	
-	// Scenario: Two include patterns, and exclude files that match a specified extension to push
+	// Scenario: 7: Two include patterns, and exclude files that match a specified extension to push
 	@When("^the include pattern specifies \\* and \\*\\* and the exclude pattern specifies \\*\\*/\\*\\.txt$")
 	public void the_include_pattern_specifies_and_and_the_exclude_pattern_specifies_txt() throws Throwable {
 		
@@ -224,6 +234,18 @@ public class UnitTestPushLocalRepo extends TestBase {
 		assertThat(this.repo.containsFile("e/a.rtf"), "e/a.rtf not found");
 		long n = this.repo.countAllFiles();
 		assertThat(n == 4, "Found " + n + " files");
+	}
+	
+	protected void setup(int dirNum) throws Exception {
+		
+		this.givendir = new File(basedir, "given" + String.valueOf(dirNum));
+		this.givendir.mkdir(); // merely returns false if directory already exists.
+		deleteDirContents(givendir);
+	}
+	
+	protected void teardown(File givendir) throws Exception {
+		deleteDirContents(givendir);
+		givendir.delete();
 	}
 	
 	protected void createDabl(String fileset) {
@@ -248,7 +270,7 @@ public class UnitTestPushLocalRepo extends TestBase {
 		LinkedList<POfilesetOperation> filesetOps = localArtifactSet.getOfilesetOperation();
 		PatternSets patternSets = new PatternSets(repo);
 		patternSets.assembleIncludesAndExcludes(getHelper(), filesetOps);
-		repo.putFiles(this.given1dir, patternSets);
+		repo.putFiles(this.givendir, patternSets);
 	}
 
 	protected void pushPatternsToRepo(String pattern) throws Exception {
@@ -263,5 +285,27 @@ public class UnitTestPushLocalRepo extends TestBase {
 		
 		// Push the task's outputs to the local repo.
 		pushOutputsToRepo(localArtifactSet, repo);
+	}
+	
+	/**
+	 * Delete all of the files and subdirectories in the specified directory.
+	 */
+	protected void deleteDirContents(File dir) throws Exception {
+		if (! file.isDirectory()) throw new Exception("File " + dir.toString() + " is not a direcrtory");
+		Stream<Path> paths = Files.walkFileTree(dir.toPath(), new SimpleFileVisitor<Path> () {
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				Files.delete(file);
+				return FileVisitResult.CONTINUE;
+			}
+			
+			public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+				if (e == null) {
+					Files.delete(dir);
+					return FileVisitResult.CONTINUE;
+				} else {
+					throw e; // directory iteration failed
+				}
+			}
+		}
 	}
 }
