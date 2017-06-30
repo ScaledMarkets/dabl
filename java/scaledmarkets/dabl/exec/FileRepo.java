ae66de1c803f98489010b02de834743772c7efa6
Cliff Berg <cliff@cliffberg.com>
2017-06-30 14:08:48 -0400

    misc

--- java/scaledmarkets/dabl/exec/FileRepo.java
+++ java/scaledmarkets/dabl/exec/FileRepo.java
@@ -3,10 +3,14 @@ package scaledmarkets.dabl.exec;
 import java.io.File;
 import java.util.List;
 import java.util.Date;
+import java.nio.file.Path;
 
 /**
  * An artifact repository.
  */
 public interface FileRepo extends Repo {
-	void tag....
+	/**
+	 * Add a tag (label) to the specified file artifact in the Repo.
+	 */
+	void addTag(Path path, String tag) throws Exception;
 }
--- java/scaledmarkets/dabl/exec/SnapshotRepo.java
+++ java/scaledmarkets/dabl/exec/SnapshotRepo.java
@@ -8,5 +8,11 @@ import java.util.Date;
  * A repo that maintains unified change history for each of a collection of files.
  */
 public interface SnapshotRepo extends Repo {
-	void tag....
+	/**
+	 * Add a tag (label) to the specified version of this Repo. The format of
+	 * the version depends on the type of Repo. For example, a git repo version
+	 * string might be a hash, or it might be an existing tag. See the implementation
+	 * note for the Repo to find out the requirements for the version parameter.
+	 */
+	void addTag(String version, String tag) throws Exception;
 }
