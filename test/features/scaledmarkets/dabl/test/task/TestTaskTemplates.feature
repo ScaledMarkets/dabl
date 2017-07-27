ec18c27456eafd4db7b1d9ca9c1d06a850158e60
Cliff Berg <cliff@cliffberg.com>
2017-07-27 08:12:26 -0400

    misc

---
+++ test/features/scaledmarkets/dabl/test/task/TestTaskTemplates.feature
@@ -0,0 +1,8 @@
+# language: en
+
+@task
+Feature: TestTaskTemplates
+	
+	Scenario: Simple
+		Given Docker is running
+		
---
+++ test/steps/scaledmarkets/dabl/test/task/TestTaskTemplates.java
@@ -0,0 +1,38 @@
+package scaledmarkets.dabl.test.task;
+
+import cucumber.api.Format;
+import cucumber.api.java.Before;
+import cucumber.api.java.After;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.And;
+
+import scaledmarkets.dabl.analyzer.*;
+import scaledmarkets.dabl.docker.*;
+import scaledmarkets.dabl.exec.*;
+import scaledmarkets.dabl.task.*;
+import scaledmarkets.dabl.node.*;
+import scaledmarkets.dabl.test.TestBase;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.List;
+import java.util.LinkedList;
+
+public class TestTaskTemplates extends TestBase {
+	
+	private String repoType;
+	CompilerState state;
+	private static String TaskName = "t123";
+
+	// Scenario: Simple
+	
+	@Given("^Docker is running$")
+	public void docker_is_running() throws Exception {
+		Docker docker = Docker.connect();
+		docker.ping();
+		docker.close();
+	}
+	
+}
