c7b46d37e9677f94dfefa18ce86d2f2481bacc32
Cliff Berg <cliff@cliffberg.com>
2017-08-26 13:40:51 -0400

    misc

--- java/scaledmarkets/dabl/analyzer/LocalRepoEntry.java
+++ java/scaledmarkets/dabl/analyzer/LocalRepoEntry.java
@@ -3,7 +3,7 @@ package scaledmarkets.dabl.analyzer;
 import scaledmarkets.dabl.node.*;
 
 /**
- * A LocalRepoEntry declared a local repository for DABL to create. Local repositories
+ * A LocalRepoEntry declares a local repository for DABL to create. Local repositories
  * contain intermediate build artifacts.
  */
 public class LocalRepoEntry extends DeclaredEntry
